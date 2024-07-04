f4012b0b7be386332702fac929ce243415be9eaa
andresGooz <gdegooz@gmail.com>
2024-07-04 15:40:30 -0400

    feat: add material design style

--- src/views/posts/post_create.js
+++ src/views/posts/post_create.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import '@material/web/textfield/outlined-text-field';
+import '@material/web/button/filled-button';
+import '@material/web/icon/icon';
+import 'material-icons/iconfont/material-icons.css';
 
 function CreatePost() {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
   const [author, setAuthor] = useState('');
-  
+
+  const titleRef = useRef();
+  const contentRef = useRef();
+  const authorRef = useRef();
+
+  useEffect(() => {
+    if (titleRef.current) {
+      titleRef.current.addEventListener('input', (e) => setTitle(e.target.value));
+    }
+    if (contentRef.current) {
+      contentRef.current.addEventListener('input', (e) => setContent(e.target.value));
+    }
+    if (authorRef.current) {
+      authorRef.current.addEventListener('input', (e) => setAuthor(e.target.value));
+    }
+  }, []);
 
   const navigate = useNavigate();
 
@@ -47,32 +65,37 @@ function CreatePost() {
       <h2>Create a New Post</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>Título:</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-          />
+          <md-outlined-text-field
+              ref={titleRef}
+              id="title"
+              label="Título"
+              value={title}
+              required
+          ></md-outlined-text-field>
         </div>
         <div>
-          <label>Contenido:</label>
-          <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            required
-          />
+          <md-outlined-text-field
+              ref={contentRef}
+              id="content"
+              label="Contenido"
+              value={content}
+              textarea
+              required
+          ></md-outlined-text-field>
         </div>
         <div>
-          <label>Autor:</label>
-          <input
-            type="text"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-            required
-          />
+          <md-outlined-text-field
+              ref={authorRef}
+              id="author"
+              label="Autor"
+              value={author}
+              required
+          ></md-outlined-text-field>
         </div>
-        <button type="submit">Create Post</button>
+        <md-filled-button type="submit">
+          <md-icon class="material-icons">create</md-icon>
+          Create Post
+        </md-filled-button>
       </form>
     </div>
   );
